35fcdb56afa052b26cf1bb79694bb0c2c29755e9
lte <lte@bringme.com>
2023-12-12 13:46:08 +0100

    actionbar component

---
+++ src/app/action-bar/action-bar.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from "@angular/core";
+import { isAndroid } from "@nativescript/core/platform";
+import { Page } from "@nativescript/core";
+import { RouterExtensions } from "@nativescript/angular";
+
+@Component({
+  selector: "ns-action-bar",
+  templateUrl: "./action-bar.component.html",
+  styleUrls: ["./action-bar.component.css"],
+})
+export class ActionBarComponent {
+  @Input()
+  title = "";
+
+  backBtnColor = "#000000";
+
+  constructor(private page: Page, private routerExtensions: RouterExtensions) {}
+
+  goBack() {
+    this.routerExtensions.backToPreviousPage();
+  }
+
+  onActionBarLoadedLoaded() {
+    if (isAndroid) {
+      let androidToolbar = this.page.actionBar.nativeView;
+      androidToolbar
+        .getNavigationIcon()
+        .setColorFilter(
+          android.graphics.Color.parseColor(this.backBtnColor),
+          (<any>android.graphics).PorterDuff.Mode.SRC_ATOP
+        );
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { HomeComponent } from "./home/home.component";
+import { ActionBarComponent } from "./action-bar/action-bar.component";
 import { AboutComponent } from "./about/about.component";
 import { GroceryListEditableComponent } from "./groceries/grocery-list-editable/grocery-list-editable.component";
 import { GroceryListTranslatedComponent } from "./groceries/grocery-list-translated/grocery-list-translated.component";
@@ -18,6 +19,7 @@ import { GroceryListComponent } from "./groceries/grocery-list/grocery-list.comp
   imports: [NativeScriptModule, AppRoutingModule],
   declarations: [
     AppComponent,
+    ActionBarComponent,
     HomeComponent,
     AboutComponent,
     GroceryListComponent,
