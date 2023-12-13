b44a170430ad908f691a27a0277a30eec92b8156
lte <lte@bringme.com>
2023-12-13 18:22:29 +0100

    add CTA btn when grocery list empty

--- src/app/groceries/grocery-list/grocery-list.component.ts
+++ src/app/groceries/grocery-list/grocery-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { AfterContentChecked, AfterViewChecked, AfterViewInit, Component, OnInit } from "@angular/core";
 import { RouterExtensions } from "@nativescript/angular";
 import { GroceryItem } from "~/app/models/grocery-item.model";
 import { ItemService } from "~/app/services/item.service";
@@ -6,13 +6,19 @@ import { ItemService } from "~/app/services/item.service";
 @Component({
   selector: "ns-grocery-list",
   templateUrl: "./grocery-list.component.html",
+  styleUrls: ["./grocery-list.component.css"],
 })
-export class GroceryListComponent {
+export class GroceryListComponent implements OnInit, AfterContentChecked {
   items: Array<GroceryItem>;
 
   constructor(private itemService: ItemService) {}
-
+  
   ngOnInit(): void {
+    this.items = this.itemService.getGroceryItemsFromStorage();
+  }
+  
+  ngAfterContentChecked(): void {
+    // on back clicked doesnt re-init page, so thi is needed
     this.items = this.itemService.getGroceryItemsFromStorage();
   }
 }
