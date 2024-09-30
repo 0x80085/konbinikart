b3fe109cab133c4b780deec6c7925455ab07758e
Lars <lte@bringme.com>
2024-09-30 13:49:23 +0200

    scripts fix translations katakana (draft)

--- scripts/replace-bad-translations.js
+++ scripts/replace-bad-translations.js
@@ -1,5 +1,54 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { cwd } = require('node:process');
 
-const curentincorrectdataset = []
+var e = require('wanakana')
+const isKatakana = e.isKatakana;
+const toKatakana = e.toKatakana;
+
+const DATA = []; // enter data here
+
+// DATA.forEach(it => {
+//     if (!isKatakana(it.nameKatakana)) {
+//         console.log(`Wrong katakana:`);
+//         console.log(it);
+
+//         console.log(`Correct katakana:`);
+//         const katakana = toKatakana(it.nameHiragana);
+//         console.log({ ...it, nameKatakana: katakana });
+//     }
+// });
+
+var correcttranslations = DATA.map(it => {
+    if (!isKatakana(it.nameKatakana)) {
+        console.log(`Wrong katakana:`);
+        console.log(it);
+
+        console.log(`Correct katakana:`);
+        const katakana = toKatakana(it.nameHiragana);
+        const corrected = { ...it, nameKatakana: katakana };
+        console.log(corrected);
+        return corrected;
+    }
+    return it;
+});
+
+const data = JSON.stringify(correcttranslations);
+const pathToFile = path.join(cwd(), 'corrected-data.json')
+if (fs.existsSync(pathToFile)) {
+    fs.rmSync(pathToFile);
+}
+fs.writeFileSync(pathToFile, data);
+
+console.log('WRTIITESN');
+
+
+//////////
+throw new Error("hah stop");
+
+
+
+const curentincorrectdataset = DATA;
 const tobereplaced = []
 
 // Function to replace bad translations
